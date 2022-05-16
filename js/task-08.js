1df2f5749aa73b5feb88036716745d73d6ba1836
Oleksandr Pavlishchev <velogitara@gmail.com>
2022-05-16 20:50:58 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const formEl = document.querySelector('.login-form');
 
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formData = new FormData(event.currentTarget);
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  const dataBase = [];
+  if (email.value === '' || password.value === '') {
+    window.alert('please, fill all form fields');
+  } else
+    formData.forEach((value, name) => {
+      //   console.log('name', name);
+      //   console.log('value', value);
+      return dataBase.push(name, value);
+    });
+  event.currentTarget.reset();
+  console.log(dataBase);
+}
