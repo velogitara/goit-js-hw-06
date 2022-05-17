9985e6cc53ead026e70d3a3208a9e5aa50cd73a8
Oleksandr Pavlishchev <velogitara@gmail.com>
2022-05-18 00:52:36 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,57 @@
+const controlsRef = document.querySelector('#controls');
+const inputValue = controlsRef.firstElementChild.value;
+
+const createBtn = document.querySelector('[data-create]');
+
+const destroyBtn = document.querySelector('[data-destroy]');
+
+const divBoxesRef = document.querySelector('#boxes');
+
+const size = '10px';
+// const newElement = document.createElement('div');
+
+// console.dir(controlsRef.firstElementChild);
+// console.log(createBtn);
+// console.log(destroyBtn);
+
+// for (var i = 1; i < 10; i++) {
+//   SummSumm = document.createElement('p');
+//   SummText = document.createTextNode('gsfdg sd sdgf sgfsgd s');
+//   SummDok[0].appendChild(SummSumm);
+//   SummSumm.appendChild(SummText);
+// }
+
+const createBoxes = event => {
+  const createFewBoxes = amount => {
+    let newElement = [];
+    for (let i = 0; i < amount; i += 1) {
+      // console.log(newElement[0]);
+
+      newElement.push(document.createElement('div'));
+    }
+    return newElement;
+  };
+
+  const result = createFewBoxes(controlsRef.firstElementChild.value);
+  result.map(el => {
+    const basicWidth = '30px';
+
+    el.classList.add('newDiv');
+    el.style.width = getBoxSize();
+    el.style.height = '30px';
+    el.style.backgroundColor = getRandomHexColor();
+
+    console.log(el);
+  });
+
+  // console.log(result);
+  divBoxesRef.append(...result);
+};
+
+createBtn.addEventListener('click', createBoxes);
+
+function getBoxSize() {}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
