0c8f4290132097393b6ebc1dba91445027d1f69b
Oleksandr Pavlishchev <velogitara@gmail.com>
2022-05-17 21:03:03 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
+const btnRef = document.querySelector('.change-color');
+const bgColorValue = document.querySelector('.color');
+
+const onClickColorChanger = () => {
+  // console.dir(element.currentTarget.offsetParent.background);
+  document.body.style.backgroundColor = getRandomHexColor();
+  // console.dir(bgColorValue.textContent);
+  console.dir(document.body.style);
+  bgColorValue.textContent = document.body.style.backgroundColor;
+};
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+btnRef.addEventListener('click', onClickColorChanger);
